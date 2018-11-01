db724779e2450748371aa320daba7858333aa87d
James Kim <jhlink@users.noreply.github.com>
2018-11-01 11:08:52 -0400

    task[10]: Call detectFacesAndOverlayEmoji within MainActivity

--- app/src/main/java/com/example/android/emojify/MainActivity.java
+++ app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -182,9 +182,9 @@ public class MainActivity extends AppCompatActivity {
         mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
         
         // Detect the faces
-        Emojifier.detectFacesAndOverlayEmoji(this, mResultsBitmap);
-        // TODO (10): Change the method call from detectFacesAndOverlayEmoji() to detectFacesAndOverlayEmoji() and assign the result to mResultsBitmap.
-        
+        // DONE (10): Change the method call from detectFaces() to detectFacesAndOverlayEmoji() and assign the result to mResultsBitmap.
+        mResultsBitmap = Emojifier.detectFacesAndOverlayEmoji(this, mResultsBitmap);
+
         // Set the new bitmap to the ImageView
         mImageView.setImageBitmap(mResultsBitmap);
     }
