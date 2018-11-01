a22f3450a5f4132c05333f88349be54f74716929
James Kim <jhlink@users.noreply.github.com>
2018-11-01 09:13:43 -0400

    fix: Handle error notification when no faces are detected in bitmap

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Log;
 import android.util.SparseArray;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
@@ -24,6 +25,11 @@ public class Emojifier {
         SparseArray<Face> faces = detector.detect(frame);
 
         Log.d(TAG, "Number of faces in bitmap: " + faces.size());
+
+        if ( faces.size() == 0 ) {
+            Toast.makeText(context, "No faces detected in bitmap.", Toast.LENGTH_SHORT);
+        }
+
         return faces.size();
     }
 }
