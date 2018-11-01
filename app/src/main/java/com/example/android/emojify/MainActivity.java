6f328aba2d2b8617beb0694bf6e1f4e2d82d5a92
James Kim <jhlink@users.noreply.github.com>
2018-11-01 09:05:10 -0400

    task[1-2]: Create Emojifier class with supporting detectFaces helper method

--- app/src/main/java/com/example/android/emojify/MainActivity.java
+++ app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -41,8 +41,8 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1): Create a Java class called Emojifier
-        // TODO (2): Create a static method in the Emojifier class called detectFaces() which detects and logs the number of faces in a given bitmap.
+    // DONE (1): Create a Java class called Emojifier
+        // DONE (2): Create a static method in the Emojifier class called detectFaces() which detects and logs the number of faces in a given bitmap.
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_STORAGE_PERMISSION = 1;
